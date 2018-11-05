e327820413c9fbcb268836fe10b7579d1ea2976f
漆谷壮真 <soma@soma-mac.local>
2018-11-05 18:05:14 +0900

    ss

--- shsc/super_exe.sh
+++ shsc/super_exe.sh
@@ -2,4 +2,4 @@
 #$ -S /bin/bash
 #$ -cwd
 
-python ../pysc/tumor_exe_plotmutation.py -av $1 -en $2 -fu2 $3 -mt $4 -po $5 -tu $6 >&1 >&2　
+python ../pysc/tumor_exe.py -av $1 -en $2 -fu2 $3 -mt $4 -po $5 -tu $6 >&1 >&2　
