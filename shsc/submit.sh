629e2d4ebf1fcd3051f72d604138120779e06615
漆谷壮真 <soma@soma-mac.local>
2018-11-05 18:39:11 +0900

    s

--- shsc/submit.sh
+++ shsc/submit.sh
@@ -2,12 +2,16 @@
 #$ -S /bin/bash
 #$ -cwd
 
-for av in ｀cat ../paratxt/av.txt｀
-  for en in ｀cat ../paratxt/en.txt｀
-    for fu2 in ｀cat ../paratxt/fu2.txt｀
-      for mt in ｀cat ../paratxt/mt.txt｀
-        for po in ｀cat ../paratxt/po.txt｀
-          for tu in ｀cat ../paratxt/tu.txt｀
-          do
-            qsub super_exe.sh $av $en $fu2 $mt $po $tu
-          done
+for av in ｀cat ../paratxt/av.txt｀;do
+for en in ｀cat ../paratxt/en.txt｀;do
+for fu2 in ｀cat ../paratxt/fu2.txt｀;do
+for mt in ｀cat ../paratxt/mt.txt｀;do
+for po in ｀cat ../paratxt/po.txt｀;do
+for tu in ｀cat ../paratxt/tu.txt｀;do
+qsub super_exe.sh $av $en $fu2 $mt $po $tu
+done
+done
+done
+done
+done
+done
