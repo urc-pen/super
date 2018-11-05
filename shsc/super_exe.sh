1d80e9c153c922058d2abaaa61f5f1c640a5fe1b
漆谷壮真 <soma@soma-mac.local>
2018-11-05 18:13:06 +0900

    x

--- shsc/super_exe.sh
+++ shsc/super_exe.sh
@@ -2,4 +2,4 @@
 #$ -S /bin/bash
 #$ -cwd
 
-python ../pysc/tumor_exe.py -av $1 -en $2 -fu2 $3 -mt $4 -po $5 -tu $6 >&1 >&2　
+python ../pysc/tumor_exe.py >&1 >&2 -av $1 -en $2 -fu2 $3 -mt $4 -po $5 -tu $6
