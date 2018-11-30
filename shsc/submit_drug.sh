8cd5b1c632a4d11840038064b505c3558831f6c9
漆谷壮真 <soma@soma-mac.local>
2018-11-30 17:28:05 +0900

    ko

--- shsc/submit_drug.sh
+++ shsc/submit_drug.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #$ -S /bin/bash
 #$ -cwd
-
+for i in 1 2 3 4 5;do
 for pi in `cat ../paratxt/pi.txt`;do
 for dr in `cat ../paratxt/dr.txt`;do
 for ef in `cat ../paratxt/ef.txt`;do
 qsub -l s_vmem=8G,mem_req=8G super_exe_drug.sh $pi $dr $ef
+done
 done
 done
 done
