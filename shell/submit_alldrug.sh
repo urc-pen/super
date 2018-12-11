53dd5eec988b65940088458e1b50c923e794a669
漆谷壮真 <soma@soma-mac.local>
2018-12-12 00:48:46 +0900

    alldrug

--- shell/submit_alldrug.sh
+++ shell/submit_alldrug.sh
@@ -2,28 +2,10 @@
 #$ -S /bin/bash
 #$ -cwd
 
-cdir=$(cd $(dirname $0) && pwd)
-hdir=${cdir%/*}
-rdir=$hdir/result/
-for i in 1 2 3 4 5 6 7 8 9 10;do
 for mt in `cat ../paratxt/mt.txt`;do
 for dr in `cat ../paratxt/dr.txt`;do
 for ef in `cat ../paratxt/ef.txt`;do
-for we1 in `cat ../paratxt/we1.txt`;do
-for we2 in `cat ../paratxt/we2.txt`;do
-fname=$rdir
-fname+=$i
-fname+=$mt
-fname+=$dr
-fname+=$ef
-fname+=$we1
-fname+=$we2
-fname=${fname//./_}
-touch $fname
-qsub -l -e $fname -o $fname s_vmem=12G,mem_req=12G super_exe_alldrug.sh $mt $dr $ef $we1 $we2 $fname
-done
-done
-done
+qsub -l s_vmem=12G,mem_req=12G super_exe_alldrug.sh $mt $dr $ef
 done
 done
 done
--- shell/super_exe_alldrug.sh
+++ shell/super_exe_alldrug.sh
@@ -3,4 +3,4 @@
 #$ -cwd
 
 
-python ../python/tumor_exe_alldrug.py >&1 >&2 -mt $1 -dr $2 -ef $3 -we1 $4 -we2 $5 -fn $6
+python ../python/tumor_exe_alldrug.py >&1 >&2 -mt $1 -dr $2 -ef $3
