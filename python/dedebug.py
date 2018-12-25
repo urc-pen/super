8460dedafbcdc2e62bd09bc7bd0ac706fe7dbbea
漆谷壮真 <soma@soma-mac.local>
2018-12-25 13:12:51 +0900

    debug

--- python/dedebug.py
+++ python/dedebug.py
@@ -1,15 +1,13 @@
+import math
 dic = {}
 list = []
-id = 1
-while id < 100000000000001:
+id = 100000000000000001
+while id != 1:
     dic[id] = 10
-    list.append(id)
-    id *= 2
+    id = math.floor(id / 2)
 
-while len(list) <= 10000000:
-     dic[id] = 10
-     list.append(id)
-
-for i in list:
-    if i not in dic.keys():
-        print(i)
+ids = 100000000000000001
+while ids != 1:
+    if ids not in dic.keys():
+        print(ids)
+    ids = math.floor(ids / 2)
